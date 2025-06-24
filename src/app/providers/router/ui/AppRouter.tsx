13c23608280eb409f9a7bdc045cc1d537c31f54d
Baiyr <emilovbaku530@gmail.com>
2025-06-24 18:24:27 +0600

    add spinner loader and fix scss files

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,25 +1,24 @@
 import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { PageLoader } from "widgets/PageLoader/PageLoader";
 
 const AppRouter = () => {
   return (
     <div>
-      <Suspense fallback={"Loading..."}>
-        <Routes>
-          {Object.values(routeConfig).map(({ path, element }) => (
-            <Route
-              key={path}
-              path={path}
-              element={
-                <Suspense fallback={<div>Loading...</div>}>
-                  <div className="page-wrapper">{element}</div>
-                </Suspense>
-              }
-            />
-          ))}
-        </Routes>
-      </Suspense>
+      <Routes>
+        {Object.values(routeConfig).map(({ path, element }) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense fallback={<PageLoader />}>
+                <div className="page-wrapper">{element}</div>
+              </Suspense>
+            }
+          />
+        ))}
+      </Routes>
     </div>
   );
 };
